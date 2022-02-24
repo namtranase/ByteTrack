baf0287e0ce86cdcabc5a19a7ffe1f96b2086611
namtranase <trannam.ase@gmail.com>
2022-02-24 09:42:05 +0700

    update: counting human

--- yolox/utils/visualize.py
+++ yolox/utils/visualize.py
@@ -63,7 +63,7 @@ def plot_tracking(image, tlwhs, obj_ids, scores=None, frame_id=0, fps=0., ids2=N
     line_thickness = 3
 
     radius = max(5, int(im_w/140.))
-    cv2.putText(im, 'frame: %d fps: %.2f num: %d' % (frame_id, fps, len(tlwhs)),
+    cv2.putText(im, 'frame: %d fps: %.2f Human count: %d' % (frame_id, fps, len(tlwhs)),
                 (0, int(15 * text_scale)), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), thickness=2)
 
     for i, tlwh in enumerate(tlwhs):
